d902555d354215bc8fabf3d42509fd9a910e74b9
remcoabalain <remco@linku.nl>
2020-04-14 21:26:21 +0200

    added jump ventures docs

---
+++ jump-ventures-components/precache-manifest.9e3e8a02d53fe3f788013b1de740ab9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d3d6e65535cd51b49c",
+    "url": "build/2.c1d3d6e6.js"
+  },
+  {
+    "revision": "c0e21e39869661db5b49d6b8a1b9b797",
+    "url": "build/2.c1d3d6e6.js.LICENSE"
+  },
+  {
+    "revision": "43aacbcfc391a21cde7ad09eb1961de8",
+    "url": "build/2.c1d3d6e6.js.LICENSE.txt"
+  },
+  {
+    "revision": "a98a5eb913b830185792",
+    "url": "build/bundle.a98a5eb9.js"
+  },
+  {
+    "revision": "765b03b58557508be4d0",
+    "url": "build/main.765b03b5.js"
+  },
+  {
+    "revision": "355535e0eea186503e93347c5f9fce1a",
+    "url": "index.html"
+  },
+  {
+    "revision": "765b03b58557508be4d0",
+    "url": "static/css/main.6bce58c1.chunk.css"
+  }
+]);
